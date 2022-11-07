232ce953ff2d78b08e7c97bf536a85d8cc2b1646
MightyHero <115051723+MightyHero@users.noreply.github.com>
2022-11-07 00:25:20 -0600

    add a div with a h1 and p tag. as well as a btn

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//questions array question,choices, answer
+//declare global variables from html
+
+
+//track time
+var qindex=0
+
+var time=questions.length * 20
+var timerid;//set interval 
+
+//create clock funtion
+//eventlistner to funtion 1
+//F1 = hide intro, unhide questions and start the clock with set interval
+
+//F2 = pull in a question, for loop create button
+
+//F3 = if statement 
