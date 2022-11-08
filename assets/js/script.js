b8c3d0c7d19528a28e2282a123c36443445f21df
MightyHero <115051723+MightyHero@users.noreply.github.com>
2022-11-07 23:51:20 -0600

    add timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,26 @@ var qindex=0
 var time=questions.length * 20
 var timerid;//set interval 
 
+var timeEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+
+function countdown() {
+    var timeLeft = 100;
+
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+            timerEl.textContent = timeLeft + ' seconds remaining';
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerEl.textContent = timeLeft + ' second remaining';
+            timeLeft--;
+        } else {
+            timerEl.textContent = '';
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+}
+
 //create clock funtion
 //eventlistner to funtion 1
 //F1 = hide intro, unhide questions and start the clock with set interval
